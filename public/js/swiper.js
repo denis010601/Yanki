c6cf6402c18f47efc57aa4da1d9a1d4819d4fd49
denis <deniska01.06.01@gmail.com>
2022-09-08 00:02:46 +0400

    Работа по боковому меню секции херо катрочки и слайдер категории добавлены ховеры

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,8 +1,25 @@
-var swiper = new Swiper(".hero__swiper", {
-    pagination: {
-      el: ".pagination-d",
-      bulletActiveClass: "activebullet",
-      bulletClass: "bullet"
+var swiper = new Swiper(".category-swiper", {
+  slidesPerView: 2,
+  spaceBetween: 5,
+  loopFillGroupWithBlank: true,
+  breakpoints: {
+    // when window width is >= 320px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 5
     },
-  });
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzd2lwZXIuanMiXSwic291cmNlc0NvbnRlbnQiOlsidmFyIHN3aXBlciA9IG5ldyBTd2lwZXIoXCIuaGVyb19fc3dpcGVyXCIsIHtcbiAgICBwYWdpbmF0aW9uOiB7XG4gICAgICBlbDogXCIucGFnaW5hdGlvbi1kXCIsXG4gICAgICBidWxsZXRBY3RpdmVDbGFzczogXCJhY3RpdmVidWxsZXRcIixcbiAgICAgIGJ1bGxldENsYXNzOiBcImJ1bGxldFwiXG4gICAgfSxcbiAgfSk7Il0sImZpbGUiOiJzd2lwZXIuanMifQ==
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 5
+    },
+    1180: {
+      slidesPerView: 4,
+      spaceBetween: 15
+    },
+  },
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  },
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzd2lwZXIuanMiXSwic291cmNlc0NvbnRlbnQiOlsidmFyIHN3aXBlciA9IG5ldyBTd2lwZXIoXCIuY2F0ZWdvcnktc3dpcGVyXCIsIHtcbiAgc2xpZGVzUGVyVmlldzogMixcbiAgc3BhY2VCZXR3ZWVuOiA1LFxuICBsb29wRmlsbEdyb3VwV2l0aEJsYW5rOiB0cnVlLFxuICBicmVha3BvaW50czoge1xuICAgIC8vIHdoZW4gd2luZG93IHdpZHRoIGlzID49IDMyMHB4XG4gICAgNDgwOiB7XG4gICAgICBzbGlkZXNQZXJWaWV3OiAzLFxuICAgICAgc3BhY2VCZXR3ZWVuOiA1XG4gICAgfSxcbiAgICA3Njg6IHtcbiAgICAgIHNsaWRlc1BlclZpZXc6IDQsXG4gICAgICBzcGFjZUJldHdlZW46IDVcbiAgICB9LFxuICAgIDExODA6IHtcbiAgICAgIHNsaWRlc1BlclZpZXc6IDQsXG4gICAgICBzcGFjZUJldHdlZW46IDE1XG4gICAgfSxcbiAgfSxcbiAgbmF2aWdhdGlvbjoge1xuICAgIG5leHRFbDogXCIuYnRuLW5leHRcIixcbiAgICBwcmV2RWw6IFwiLmJ0bi1wcmV2XCIsXG4gIH0sXG59KTsiXSwiZmlsZSI6InN3aXBlci5qcyJ9
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all elements in your page
+
+
 let select = function () {
     let selectHeader = document.querySelectorAll('.select__header');
     let selectItem = document.querySelectorAll('.select__item');
+    let selectBody = document.querySelectorAll('.select__body');
 
     selectHeader.forEach(item => {
-        item.addEventListener('click', selectToggle)
+        item.addEventListener('click', selectToggle 
+        )
     });
-
     selectItem.forEach(item => {
         item.addEventListener('click', selectChoose)
     });
@@ -13,6 +18,7 @@ let select = function () {
     function selectToggle() {
         this.parentElement.classList.toggle('is-active');
     }
+
 
     function selectChoose() {
         let text = this.innerText,
@@ -30,9 +36,12 @@ select();
 
 let burger = document.getElementById('burger');
     burgerMenu = document.getElementById('burgerMenu');
-
+    logo = document.getElementById('logo');
+    navList = document.getElementById('navList');
 burger.onclick = function() {
     burger.classList.toggle('is-active');
     burgerMenu.classList.toggle('is-active');
-    
+    logo.classList.toggle('is-active');
+    navList.classList.toggle('is-active');
 };
+});
